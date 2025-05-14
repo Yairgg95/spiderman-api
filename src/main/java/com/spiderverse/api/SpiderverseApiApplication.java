020e74589c1dc697c5fdd2dcf5a0a955d0ca7e87
Yairgg95 <yair.g.guadarrama@icloud.com>
2025-05-14 15:30:49 -0600

    First commit

---
+++ src/main/java/com/spiderverse/api/SpiderverseApiApplication.java
@@ -0,0 +1,13 @@
+package com.spiderverse.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderverseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderverseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spiderverse/api/SpiderverseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spiderverse.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiderverseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
