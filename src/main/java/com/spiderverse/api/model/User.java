002e198a803701e5705b4171821f1ce0df7de834
Yairgg95 <yair.g.guadarrama@icloud.com>
2025-05-15 00:49:59 -0600

    [FEAT] add User model

---
+++ src/main/java/com/spiderverse/api/model/User.java
@@ -0,0 +1,20 @@
+package com.spiderverse.api.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String userName;
+
+    private String password;
+}
