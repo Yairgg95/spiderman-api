ffad8c96d026a5106fd4e2868770195591a8094d
Yairgg95 <yair.g.guadarrama@icloud.com>
2025-05-15 01:49:45 -0600

    [FEAT] add JwtUtil

---
+++ src/main/java/com/spiderverse/api/security/JwtUtil.java
@@ -0,0 +1,57 @@
+package com.spiderverse.api.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private Key key;
+    private final long expitation = 1000 * 60 * 60 * 10;
+
+    @PostConstruct
+    public void init() {
+        String secret = System.getenv("JWT_SECRET");
+        if (secret == null || secret.isEmpty()) {
+            throw new IllegalStateException("JWT_SECRET enviroment variable not set.");
+        }
+
+        byte[] decodedKey = Base64.getDecoder().decode(secret);
+        this.key = Keys.hmacShaKeyFor(decodedKey);
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expitation))
+                .signWith(key)
+                .compact();
+    }
+
+    private Claims parseClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractUsername(String token) {
+        return parseClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            parseClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
