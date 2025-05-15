8dc15af46e024194ec168be7f78a62105dae9a38
Yairgg95 <yair.g.guadarrama@icloud.com>
2025-05-15 02:04:23 -0600

    [FEAT] add Auth service

---
+++ src/main/java/com/spiderverse/api/security/JwtAutheticationFilter.java
@@ -0,0 +1,4 @@
+package com.spiderverse.api.security;
+
+public class JwtAutheticationFilter {
+}
--- src/main/java/com/spiderverse/api/service/AuthService.java
+++ src/main/java/com/spiderverse/api/service/AuthService.java
@@ -1,4 +1,46 @@
 package com.spiderverse.api.service;
 
+import com.spiderverse.api.dto.UserResponseDTO;
+import com.spiderverse.api.model.User;
+import com.spiderverse.api.repository.UserRepository;
+import com.spiderverse.api.security.JwtUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public UserResponseDTO register(String username, String rawPassword){
+        if(userRepository.findByUsername(username).isPresent()) {
+            throw new RuntimeException("Username already exists");
+        }
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(rawPassword));
+        userRepository.save(user);
+
+        return new UserResponseDTO(user.getUsername());
+    }
+
+    public String login(String username, String rawPassword) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Invalid credentials"));
+
+        if(!passwordEncoder.matches(rawPassword, user.getPassword())) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        return jwtUtil.generateToken(username);
+    }
 }
